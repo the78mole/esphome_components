0351ee71f856eb13ea8494bfd8c501121aa76219
Bascht74 <sebastian.n.lotz@gmail.com>
2022-12-09 23:13:09 +0100

    add missing comma

--- components/km271_wifi/binary_sensor.py
+++ components/km271_wifi/binary_sensor.py
@@ -32,19 +32,19 @@ CONFIG_SCHEMA = (
         {
             cv.GenerateID(CONF_KM271_ID): cv.use_id(KM271),
             cv.Optional("load_pump_running"): binary_sensor.binary_sensor_schema(
-                device_class=DEVICE_CLASS_RUNNING
-                entity_category=ENTITY_CATEGORY_DIAGNOSTIC, 	
+                device_class=DEVICE_CLASS_RUNNING,
+                entity_category=ENTITY_CATEGORY_DIAGNOSTIC,	
             ),
             cv.Optional("boiler_error"): binary_sensor.binary_sensor_schema(
                 device_class=DEVICE_CLASS_PROBLEM,
                 entity_category=ENTITY_CATEGORY_DIAGNOSTIC, 	
             ),
             cv.Optional("boiler_running"): binary_sensor.binary_sensor_schema(
-                device_class=DEVICE_CLASS_RUNNING
+                device_class=DEVICE_CLASS_RUNNING,
                 entity_category=ENTITY_CATEGORY_DIAGNOSTIC, 	
             ),
             cv.Optional("boiler_actuation"): binary_sensor.binary_sensor_schema(
-                device_class=DEVICE_CLASS_POWER
+                device_class=DEVICE_CLASS_POWER,
                 entity_category=ENTITY_CATEGORY_DIAGNOSTIC, 
             ),
         }
