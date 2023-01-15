dfe976a3e85ddb78814a6a13ff616ecf0d3a6acb
Quentin Schneider <3471813+qschneider@users.noreply.github.com>
2023-01-15 15:01:30 +0100

    Update number.py
    fixed missing closing bracket

--- components/km271_wifi/number.py
+++ components/km271_wifi/number.py
@@ -65,7 +65,7 @@ CONFIG_SCHEMA = (
                 cv.Optional(CONF_MAX_VALUE, default=90): cv.float_,
                 cv.Optional(CONF_MIN_VALUE, default=20): cv.float_,
                 cv.Optional(CONF_STEP, default=1): cv.positive_float,
-                
+            }),                
             cv.Optional(CONF_HC2_DESIGN_TEMP): number.NUMBER_SCHEMA.extend({
                 cv.GenerateID(): cv.declare_id(BuderusParamNumber),
                 cv.Optional(CONF_MAX_VALUE, default=90): cv.float_,
