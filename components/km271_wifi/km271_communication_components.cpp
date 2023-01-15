7c1c3304eff605cb1710ec9ffa4efd34b32c674e
Quentin Schneider <3471813+qschneider@users.noreply.github.com>
2023-01-15 14:23:30 +0100

    Update km271_communication_components.cpp
    fixed spacing

--- components/km271_wifi/km271_communication_components.cpp
+++ components/km271_wifi/km271_communication_components.cpp
@@ -81,8 +81,6 @@ void BuderusParamNumber::control(float value)
        this->pendingWriteValue = limitValueToRange(value, -5, 5);
     } else if(transmissionParameter == config_heating_circuit_2_flow_target_temperature_max) {
         this->pendingWriteValue = limitValueToRange(value, 20, 90);
-
-
     } else {
         ESP_LOGE(TAG, "No write configuration for number transmission parameter %d found", transmissionParameter);
         return;
@@ -113,30 +111,28 @@ void BuderusParamNumber::loop()
     if (this->hasPendingWriteRequest) {
         uint32_t now = millis();
         if (now - lastWriteRequest > writeConsolidationPeriod) {
-
-          if (transmissionParameter == config_ww_temperature) {
-            const uint8_t message[] = { data_type_warm_water, 0x07, keep, keep, keep, (uint8_t)this->pendingWriteValue, keep, keep};
-            sendAndConfirm(message, sizeof(message));
-          } else if(transmissionParameter == config_heating_circuit_1_design_temperature) {
-              const uint8_t message[] = { data_type_heating_circuit_1, 0x0e, keep, keep, keep, keep, (uint8_t)this->pendingWriteValue, keep};
-              sendAndConfirm(message, sizeof(message));
-          } else if(transmissionParameter == config_heating_circuit_1_target_room_temperature_day) {
-              const uint8_t message[] = { data_type_heating_circuit_1, 0x0, keep, keep, keep, (uint8_t)(this->pendingWriteValue * 2 + 0.5), keep, keep};
-              sendAndConfirm(message, sizeof(message));
-
-          } else if(transmissionParameter == config_heating_circuit_2_design_temperature) {
-              const uint8_t message[] = { data_type_heating_circuit_2, 0x0e, keep, keep, keep, keep, (uint8_t)this->pendingWriteValue, keep};
-              sendAndConfirm(message, sizeof(message));
-          } else if(transmissionParameter == config_heating_circuit_2_room_target_temperature_day) {
-              const uint8_t message[] = { data_type_heating_circuit_2, 0x0, keep, keep, keep, (uint8_t)(this->pendingWriteValue * 2 + 0.5), keep, keep};
-              sendAndConfirm(message, sizeof(message));
-          } else if(transmissionParameter == config_heating_circuit_2_room_target_temperature_offset) {
-              const uint8_t message[] = { data_type_heating_circuit_2, 0x31, keep, keep, keep, (uint8_t)(this->pendingWriteValue * 2), keep, keep};
-              sendAndConfirm(message, sizeof(message));
-          } else if(transmissionParameter == config_heating_circuit_2_flow_target_temperature_max) {
-              const uint8_t message[] = { data_type_heating_circuit_2, 0x0e, keep, keep, (uint8_t)this->pendingWriteValue, keep, keep, keep};
-              sendAndConfirm(message, sizeof(message));
-          } else {
+            if (transmissionParameter == config_ww_temperature) {
+                const uint8_t message[] = { data_type_warm_water, 0x07, keep, keep, keep, (uint8_t)this->pendingWriteValue, keep, keep};
+                sendAndConfirm(message, sizeof(message));
+            } else if(transmissionParameter == config_heating_circuit_1_design_temperature) {
+                const uint8_t message[] = { data_type_heating_circuit_1, 0x0e, keep, keep, keep, keep, (uint8_t)this->pendingWriteValue, keep};
+                sendAndConfirm(message, sizeof(message));
+            } else if(transmissionParameter == config_heating_circuit_1_target_room_temperature_day) {
+                const uint8_t message[] = { data_type_heating_circuit_1, 0x0, keep, keep, keep, (uint8_t)(this->pendingWriteValue * 2 + 0.5), keep, keep};
+                sendAndConfirm(message, sizeof(message));
+            } else if(transmissionParameter == config_heating_circuit_2_design_temperature) {
+                const uint8_t message[] = { data_type_heating_circuit_2, 0x0e, keep, keep, keep, keep, (uint8_t)this->pendingWriteValue, keep};
+                sendAndConfirm(message, sizeof(message));
+            } else if(transmissionParameter == config_heating_circuit_2_room_target_temperature_day) {
+                const uint8_t message[] = { data_type_heating_circuit_2, 0x0, keep, keep, keep, (uint8_t)(this->pendingWriteValue * 2 + 0.5), keep, keep};
+                sendAndConfirm(message, sizeof(message));
+            } else if(transmissionParameter == config_heating_circuit_2_room_target_temperature_offset) {
+                const uint8_t message[] = { data_type_heating_circuit_2, 0x31, keep, keep, keep, (uint8_t)(this->pendingWriteValue * 2), keep, keep};
+                sendAndConfirm(message, sizeof(message));
+            } else if(transmissionParameter == config_heating_circuit_2_flow_target_temperature_max) {
+                const uint8_t message[] = { data_type_heating_circuit_2, 0x0e, keep, keep, (uint8_t)this->pendingWriteValue, keep, keep, keep};
+                sendAndConfirm(message, sizeof(message));
+            } else {
                 ESP_LOGE(TAG, "No support for writing transmission parameter %d", transmissionParameter);
                 this->hasPendingWriteRequest = false;
             }
