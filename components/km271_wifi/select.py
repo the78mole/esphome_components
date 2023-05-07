1d9675d7b6762dde87c9dbbcb971c70bc0023481
Quentin Schneider <3471813+qschneider@users.noreply.github.com>
2023-05-07 12:08:13 +0200

    Update select.py

--- components/km271_wifi/select.py
+++ components/km271_wifi/select.py
@@ -16,14 +16,16 @@ from .const import *
 CODEOWNERS = ["@the78mole", "@jensgraef"]
 
 TYPES = [
-    CONF_SWT_SWITCH,
+
 #    CONF_BLR_BUILDING_TYPE,
-    CONF_HC1_OPMODE,
+    CONF_HC1_SWT_SWITCH,
     CONF_HC1_HEATING_PROGRAM,
+    CONF_HC1_OPMODE,
     CONF_HC1_LOWERING_TYPE,
     CONF_HC1_HEATINGS_TYPE,
-    CONF_HC2_OPMODE,
+    CONF_HC2_SWT_SWITCH
     CONF_HC2_HEATING_PROGRAM,
+    CONF_HC2_OPMODE,
     CONF_HC2_LOWERING_TYPE,
     CONF_HC2_HEATINGS_TYPE,
     CONF_WW_OPMODE,
@@ -56,17 +58,17 @@ def ensure_option_map(value):
 CONFIG_SCHEMA = (
     cv.Schema(
         {
-            cv.GenerateID(CONF_KM271_ID): cv.use_id(KM271),
-            cv.Optional(CONF_SWT_SWITCH): select.SELECT_SCHEMA.extend({
-                cv.GenerateID(): cv.declare_id(BuderusParamSelect),
-                cv.Optional(CONF_OPTIONS, default={9: 'Sommer', 10: '10°C', 11: '11°C', 12: '12°C', 13: '13°C', 14: '14°C', 15: '15°C', 16: '16°C', 17: '17°C', 18: '18°C', 19: '19°C', 20: '20°C', 21: '21°C', 22: '22°C', 23: '23°C', 24: '24°C', 25: '25°C', 26: '26°C', 27: '27°C', 28: '28°C', 29: '29°C', 30: '30°C', 31: 'Winter'}): ensure_option_map
-            }),
 #            cv.GenerateID(CONF_KM271_ID): cv.use_id(KM271),
 #            cv.Optional(CONF_BLR_BUILDING_TYPE): select.SELECT_SCHEMA.extend({
 #                cv.GenerateID(): cv.declare_id(BuderusParamSelect),
 #                cv.Optional(CONF_OPTIONS, default={0: 'Leicht', 1: 'Mittel', 2: 'Schwer'}): ensure_option_map
 #            }),
 
+            cv.GenerateID(CONF_KM271_ID): cv.use_id(KM271),
+            cv.Optional(CONF_HC1_SWT_SWITCH): select.SELECT_SCHEMA.extend({
+                cv.GenerateID(): cv.declare_id(BuderusParamSelect),
+                cv.Optional(CONF_OPTIONS, default={9: 'Sommer', 10: '10°C', 11: '11°C', 12: '12°C', 13: '13°C', 14: '14°C', 15: '15°C', 16: '16°C', 17: '17°C', 18: '18°C', 19: '19°C', 20: '20°C', 21: '21°C', 22: '22°C', 23: '23°C', 24: '24°C', 25: '25°C', 26: '26°C', 27: '27°C', 28: '28°C', 29: '29°C', 30: '30°C', 31: 'Winter'}): ensure_option_map
+            }),
             cv.GenerateID(CONF_KM271_ID): cv.use_id(KM271),
             cv.Optional(CONF_HC1_HEATING_PROGRAM): select.SELECT_SCHEMA.extend({
                 cv.GenerateID(): cv.declare_id(BuderusParamSelect),
@@ -88,6 +90,11 @@ CONFIG_SCHEMA = (
                 cv.Optional(CONF_OPTIONS, default={0: 'Aus', 1: 'Heizkörper'}): ensure_option_map
             }),
 
+            cv.GenerateID(CONF_KM271_ID): cv.use_id(KM271),
+            cv.Optional(CONF_HC2_SWT_SWITCH): select.SELECT_SCHEMA.extend({
+                cv.GenerateID(): cv.declare_id(BuderusParamSelect),
+                cv.Optional(CONF_OPTIONS, default={9: 'Sommer', 10: '10°C', 11: '11°C', 12: '12°C', 13: '13°C', 14: '14°C', 15: '15°C', 16: '16°C', 17: '17°C', 18: '18°C', 19: '19°C', 20: '20°C', 21: '21°C', 22: '22°C', 23: '23°C', 24: '24°C', 25: '25°C', 26: '26°C', 27: '27°C', 28: '28°C', 29: '29°C', 30: '30°C', 31: 'Winter'}): ensure_option_map
+            }),
             cv.GenerateID(CONF_KM271_ID): cv.use_id(KM271),
             cv.Optional(CONF_HC2_HEATING_PROGRAM): select.SELECT_SCHEMA.extend({
                 cv.GenerateID(): cv.declare_id(BuderusParamSelect),
